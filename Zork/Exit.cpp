1166d26fd9e4436aac71b545e4d94164d6e3fbb4
Marc López <marclop.se@gmail.com>
2024-02-17 13:46:15 +0100

    Exit class implementation
    Has been implemented Exit class with constructor & detect opposite direction. Used index of commands to stabilize the strings of directions.

--- Zork/Exit.cpp
+++ Zork/Exit.cpp
@@ -1,1 +1,35 @@
 #include "Exit.h"
+
+
+Exit::Exit(const std::string& dir, const std::string& nameExit, const std::string& descriptionExit, Entity* parent1, Entity* parent2)
+	:Entity(nameExit, descriptionExit,parent1)
+{
+	dirSource = dir;
+	detectOpositeDir(dir);
+
+	source = (Room*)parent1;
+	destination = (Room*)parent2;
+
+	if (destination != nullptr)
+		destination->containerEntities.push_back(this);
+}
+void Exit::detectOpositeDir(const std::string& dirSource)
+{
+	if (dirSource == nameComm.NORTH_1 || dirSource == nameComm.NORTH_2)
+		dirDestination = nameComm.SOUTH_1;
+
+	else if (dirSource == nameComm.SOUTH_1 || dirSource == nameComm.SOUTH_2)
+		dirDestination = nameComm.NORTH_1;
+
+	else if (dirSource == nameComm.EAST_1 || dirSource == nameComm.EAST_2)
+		dirDestination = nameComm.WEST_1;
+
+	else if (dirSource == nameComm.WEST_1 || dirSource == nameComm.WEST_2)
+		dirDestination = nameComm.EAST_1;
+
+	else if (dirSource == nameComm.UP_1 || dirSource == nameComm.UP_2)
+		dirDestination = nameComm.DOWN_1;
+
+	else if (dirSource == nameComm.DOWN_1 || dirSource == nameComm.DOWN_2)
+		dirDestination = nameComm.UP_1;
+}
--- Zork/Exit.h
+++ Zork/Exit.h
@@ -3,13 +3,24 @@
 
 #include "Entity.h"
 #include "Room.h"
+#include "NameCommands.h"
+
+NameCommands nameComm;
 
 class Exit : public Entity
 {
 public:
 
+	Exit(const std::string& dir, const std::string& nameExit, const std::string& descriptionExit, Entity* parent1, Entity* parent2);
+
+	void detectOpositeDir(const std::string& dirSource);
+
+	std::string dirSource;
+	std::string dirDestination;
+
 	Room* source;
 	Room* destination;
+
 };
 
 #endif //Exit_h
