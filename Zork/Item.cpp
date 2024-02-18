3db08e36a3fe3a7013a37c49ec444defb67bff3e
Marc López <marclop.se@gmail.com>
2024-02-18 13:31:08 +0100

    Item class implemented

--- Zork/Item.cpp
+++ Zork/Item.cpp
@@ -1,1 +1,17 @@
 #include "Item.h"
+
+Item::Item(const std::string& nameItem, const std::string& descriptItem, Entity* parentItem, bool isAContainer, int valueItem)
+	:Entity(nameItem, descriptItem, parentItem)
+{
+	isItemAContainer = isAContainer;
+	valuePoints = valueItem;
+}
+
+Item::~Item()
+{
+}
+
+void Item::look()
+{
+	std::cout << "Item: " + name + "\n" + description << std::endl;
+}
--- Zork/Item.h
+++ Zork/Item.h
@@ -5,6 +5,15 @@
 
 class Item : public Entity
 {
+public:
+
+	Item(const std::string& nameItem, const std::string& descriptItem, Entity* parentItem, bool isAContainer, int valueItem = 0);
+	~Item();
+
+	void look() override;
+
+	bool isItemAContainer;
+	int valuePoints;
 };
 
 #endif //Item_h
